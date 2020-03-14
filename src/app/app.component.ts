e6d172429ffe2b3f91b7dc4ffc62c49d660a20e5
aramkhasro <aram@24group.net>
2020-03-14 22:36:25 +0300

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component ,AfterViewInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(){
+    firebase.analytics().logEvent('eventname',{
+    'firsttimuser':true,
+    'username':'craftech'
+    })
+  }
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import * as firebase from 'firebase/app';
+import {environment} from '../environments/environment'
+firebase.initializeApp(environment.firebaseConfig)
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
