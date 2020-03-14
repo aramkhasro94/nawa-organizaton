a8ace7c73027ae6089e793417cfd20c4e02901e5
aramkhasro <aram@24group.net>
2020-03-14 22:52:10 +0300

    abc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import * as firebase from 'firebase/app';
-import {environment} from '../environments/environment'
-firebase.initializeApp(environment.firebaseConfig);
+import {environment} from '../environments/environment';
+firebase.initializeApp(environment);
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
