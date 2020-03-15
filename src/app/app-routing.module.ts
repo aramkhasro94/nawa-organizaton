ef0fd2fe165e04a9c7cca67291d75ce4f7c886bf
aramkhasro <aram@24group.net>
2020-03-15 16:44:26 +0300

    master update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefinitionComponent } from './definition/definition.component';
+import { ActivityComponent } from './activity/activity.component';
 
 
 const routes: Routes = [
+  {path:'definition',component:DefinitionComponent},
+  {path:'activity',component:ActivityComponent},
+  {path:'',redirectTo:'/activity',pathMatch:'full'}
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { DefinitionComponent } from './definition/definition.component';
+import { ActivityComponent } from './activity/activity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    DefinitionComponent,
+    ActivityComponent
   ],
   imports: [
     BrowserModule,
